bd4625e22d5b0fac512917b7120c6aa07b860660
EV Rodriguez-Greenfield <estela.rodriguez-greenfield@my.utsa.edu>
2021-11-07 18:49:31 -0600

    Added the rest of the skeleton views. All controllers have "back" button handler.

--- CalculatorCompendium/src/application/controller/MatrixController.java
+++ CalculatorCompendium/src/application/controller/MatrixController.java
@@ -6,6 +6,31 @@
 
 package application.controller;
 
+import application.Main;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+
 public class MatrixController {
 
+	@FXML
+    private Button btnBack;
+
+    @FXML
+    void handleBack(ActionEvent event) {    	
+    	try {
+
+			Parent root = FXMLLoader.load(getClass().getResource("../view/Main.fxml"));
+			((Stage) Main.stage).setScene(new Scene(root, 800, 800));
+			((Stage) Main.stage).show();
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+    }
 }
