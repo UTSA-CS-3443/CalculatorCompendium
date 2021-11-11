ef872279ef109a41873fa2a2c56815689f92af27
EV Rodriguez-Greenfield <estela.rodriguez-greenfield@my.utsa.edu>
2021-11-11 13:49:47 -0600

    Added Tax View basics and copied sample skeleton to controller.

--- CalculatorCompendium/src/application/controller/TaxController.java
+++ CalculatorCompendium/src/application/controller/TaxController.java
@@ -13,15 +13,54 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.TextField;
 
 public class TaxController {
-	
-	@FXML
-    private Button btnBack;
 
-    @FXML
-    void handleBack(ActionEvent event) {    	
-    	try {
+	@FXML
+	private ComboBox<?> ddLocation;
+
+	@FXML
+	private Button btnBack;
+
+	@FXML
+	private TextField txtHouseholdIncome;
+
+	@FXML
+	private TextField txtDeductions;
+
+	@FXML
+	private Button btnCalculate;
+
+	@FXML
+	private TextField txtIRA;
+
+	@FXML
+	private Button btnClear;
+
+	@FXML
+	private TextField txt401k;
+
+	@FXML
+	private TextField txtExceptions;
+
+	@FXML
+	private ComboBox<?> ddFilingStatus;
+
+	@FXML
+	void handleClear(ActionEvent event) {
+
+	}
+
+	@FXML
+	void handleCalculate(ActionEvent event) {
+
+	}
+
+	@FXML
+	void handleBack(ActionEvent event) {
+		try {
 
 			Parent root = FXMLLoader.load(getClass().getResource("../view/Main.fxml"));
 			(Main.stage).setScene(new Scene(root, 800, 800));
@@ -31,6 +70,6 @@ public class TaxController {
 			e.printStackTrace();
 		}
 
-    }
+	}
 
 }
