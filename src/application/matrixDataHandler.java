8665a3adb51ad345207a48019d76fc23b059f48e
Brandon <brandon_stevenson01@hotmail.com>
2021-11-18 14:40:59 -0600

    added little changes to functions.

--- src/application/matrixDataHandler.java
+++ src/application/matrixDataHandler.java
@@ -1,13 +1,27 @@
 package application;
 
 public class matrixDataHandler {
-
+    //Global variables
+    private int[][] matrixA, matrixB;
+    private int rowA, rowB, columnA, columnB;
     //Constructor
     public matrixDataHandler(){
 
     }
 
     /*
-    // 
+    - This function will take in a matrix created in the matrixController upon user button press.
+    - This function will determine if the matrix is regular or irregular.
+    - If the matrix is irregular then the function will return an error that will be printed into the result area.
+      //UPDATE
+      - These two functions will count the amount of rows and columns. If the previous amount
+      of values in one row/column is not the same as the last then the function returns false
      */
+    public boolean findRows(int[][] matrixA, int[][] matrixB){
+        return true;
+    }
+
+    public boolean findColumns(int[][] matrixA, int[][] matrixB){
+        return true;
+    }
 }
