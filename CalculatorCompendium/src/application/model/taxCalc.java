8ba234c791aceed7cf367ded4658c0e25742142a
Don <ayesh98@gmail.com>
2021-11-26 02:44:44 -0600

    tax calculator setup

---
+++ CalculatorCompendium/src/application/model/taxCalc.java
@@ -0,0 +1,19 @@
+package application.model;
+
+public class taxCalc {
+	
+	private int householdIncome;
+	private int contribution401k;
+	private int contributionIRA;
+	private int deductions;
+	private int numTaxExceptions;
+	
+	public taxCalc() {
+		
+	}
+	
+	public void loadTaxRates() {
+		
+	}
+	
+}
